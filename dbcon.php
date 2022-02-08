8abbf191930d0b2ce6ae138913721ec04aa47f9c
FahathAMM <fahathammex90@gmail.com>
2022-02-08 15:34:25 +0530

    intial commit

--- dbcon.php
+++ dbcon.php
@@ -1,4 +1,4 @@
 
 <?php
-mysql_select_db('house',mysql_connect('localhost','root',''))or die(mysql_error());
+mysql_select_db('house',mysql_connect('localhost','root',''))or die(mysql_error()); 
 ?>
